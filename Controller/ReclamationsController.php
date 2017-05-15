93b259cfbbf9d681f853e183d0c20865b67c8b01
fredy Bejarano <fredy.bejarano@agenciadetierras.gov.co>
2017-05-15 11:09:54 -0500

    Se modifica para que solo se acepte un registro de reclamación por persona

--- Controller/ReclamationsController.php
+++ Controller/ReclamationsController.php
@@ -18,42 +18,47 @@ class ReclamationsController extends AppController {
             $this->set('candidate', $candidate);
             $this->set('hash_documento', $hash_documento);
         } else {
-
-            $parameters[] = '"' . $this->data['Reclamation']['hash_documento'] . '"';
-            $parameters[] = '"' . $this->data['Reclamation']['justificacion'] . '"';
-
-            $parametros_string = $separado_por_comas = implode(",", $parameters);
-
-
-            $reclamacion_id = $this->Reclamation->query('EXEC CONVOCATORIA_INSCRIPCION.dbo.newReclamation ' . $parametros_string . ';');
-
-            //var_dump($reclamacion_id); exit;
-
-
+            $file_error = false;
             $rutaArchivo = APP . "webroot" . DS . "files" . DS . "Reclamaciones" . DS . $this->data['Reclamation']['NRODOCUMENTO'];
             if (!is_dir($rutaArchivo)) {
                 if (!mkdir($rutaArchivo)) {
-                    echo "error creando archivo";
-                    $this->redirect(array('controller' => 'Reclamations', 'action' => "end"));
+                    $file_error = true;
                 }
             }
+            if ($this->data['Reclamation']["error"] == 1) {
+                $file_error = true;
+            }
+            if (!$file_error) {
+                $parameters[] = '"' . $this->data['Reclamation']['hash_documento'] . '"';
+                $parameters[] = '"' . $this->data['Reclamation']['justificacion'] . '"';
+                $parametros_string = $separado_por_comas = implode(",", $parameters);
 
-            if (!empty($this->data['Reclamation']['archivo']['tmp_name'])) {
-                $nombrearchivo = "Reclamacion-" . $reclamacion_id[0][0]['id'] . ".pdf";
-                if (move_uploaded_file($this->data['Reclamation']['archivo']['tmp_name'], $rutaArchivo . DS . $nombrearchivo)) {
-                    //$this->Session->setFlash('Archivo cargado exitosamente', 'flash');
-                    $this->redirect(array('action' => 'end'));
+                //si se obtiene como respuesta 0 significa que ya existia una reclamacion por esa cédula y no se crea el archivo
+                $reclamacion_id = $this->Reclamation->query('EXEC CONVOCATORIA_INSCRIPCION.dbo.newReclamation ' . $parametros_string . ';');
+
+                if ($reclamacion_id[0][0]['id'] == 0) {
+                    $this->redirect(array('action' => 'end', "Ya existe una reclamación para esta cédula"));
+                }
+
+                if (!empty($this->data['Reclamation']['archivo']['tmp_name'])) {
+                    $nombrearchivo = "Reclamacion-" . $reclamacion_id[0][0]['id'] . ".pdf";
+                    if (move_uploaded_file($this->data['Reclamation']['archivo']['tmp_name'], $rutaArchivo . DS . $nombrearchivo)) {
+                        $this->redirect(array('action' => 'end', "Archivo guardado exitosamente"));
+                    } else {
+                        $this->redirect(array('action' => 'end', "Error adjuntando archivo"));
+                    }
                 } else {
-                    $this->Session->setFlash('Error adjuntando archivo, por favor intentelo nuevamente ', 'flash_error');
+                    $this->redirect(array('action' => 'end', "Registro guardado exitosamente sin archivo"));
                 }
             } else {
-                $this->redirect(array('action' => 'end'));
+                $this->redirect(array('action' => 'end', "Error adjuntando archivo"));
             }
         }
     }
 
-    public function end() {
+    public function end($mensaje) {
         $this->layout = "reclamation";
+        $this->set('mensaje', $mensaje);
     }
 
 }
